a12df7415fb069f8c0b4e6c1df829fa5cf3de4f9
Rasega <khsb652@astrazeneca.net>
2020-05-19 08:40:11 +0530

    added Text_wrap solution

---
+++ text_wrap.py
@@ -0,0 +1,11 @@
+import textwrap
+
+def wrap(string, max_width):
+    l= list()
+    for i in range(0,len(string),max_width): l.append(string[i:i+max_width])
+    return "\n".join(l)
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
